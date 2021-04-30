60c03680bc596fc21af8606fa4c4b2f942d3569b
adrianprimo <adrian_1498@yahoo.com.mx>
2021-04-30 15:39:33 -0500

    Primer commit

---
+++ models/server.js
@@ -0,0 +1,48 @@
+//Servidor de express
+const express = require('express')
+const http = require('http')
+const socketio = require('socket.io')
+const path = require('path')
+
+const Sockets = require('./sockets')
+
+class Server {
+
+    constructor() {
+
+        this.app = express()
+        this.port = process.env.PORT;
+        // this.port = 8080
+
+        //Http Server
+        this.server = http.createServer(this.app)
+
+        //Configuracion sockets
+        this.io = socketio( this.server, { /* configuraciones */ } )
+    }
+
+    middlewares() {
+        // Desplegar el directorio público
+        this.app.use( express.static( path.resolve( __dirname, '../public' ) ) );
+    }
+
+    configurarSockets() {
+        new Sockets( this.io )
+    }
+
+    execute() {
+        //Inicializar Middlewares
+        this.middlewares()
+
+        //Inicializar sockets
+        this.configurarSockets()
+
+        //Inicializar Server
+        this.server.listen(this.port, () => {
+            console.log('Server corriendo en el puerto:', this.port)
+        });
+    }
+}
+
+
+module.exports = Server
